9a4a244989d7011aa0d1774f208e4f61d5cc9ee5
DaronP <per16362@uvg.edu.gt>
2018-05-25 20:26:36 -0600

    Opcion 5

--- Main.py
+++ Main.py
@@ -15,14 +15,14 @@ user.add(u3)
 #Se crea los restaurantes y hacemos sus categorias 
 restaurantes = db.labels.create("Restaurantes")
 r1 = db.nodes.create(name="Tre Fratelli", price="Alto", type1="Comida Italiana", tel="2493 8037", zone="11", parking="Gratis", saludable="S", timing="Tenedor")
-r2 = db.nodes.create(name="Carls Jr", price="Medio", type1="Hamburguesas", tel="2361 8052", zone="11", parking="Gratis", saludable="NS", timing="Rapida")
+r2 = db.nodes.create(name="Carls Jr", price="Medio", type1="Hamburguesas", tel="2361 8052", zone="7", parking="Gratis", saludable="NS", timing="Rapida")
 r3 = db.nodes.create(name="Subway", price="Bajo", type1="Panes", tel="2386 8686", zone="12", parking="Gratis", saludable="S", timing="Rapida")
-r4 = db.nodes.create(name="Tapas y Canas", price="Alto", type1="Comida Española", tel="2473 7779", zone="10", parking="Pagado", saludable="S", timing="Tenedor")
+r4 = db.nodes.create(name="Tapas y Canas", price="Alto", type1="Comida Española", tel="2473 7779", zone="15", parking="Pagado", saludable="S", timing="Tenedor")
 r5 = db.nodes.create(name="Taco Bell", price="Bajo", type1="Comida Mexicana", tel="2202 0000", zone="16", parking="Gratis", saludable="NS", timing="Rapida")
-r6 = db.nodes.create(name="Barista", price="Medio", type1="Café y postres", tel="2411 7272", zone="11", parking="Gratis", saludable="S", timing="Rapida")
+r6 = db.nodes.create(name="Barista", price="Medio", type1="Café y postres", tel="2411 7272", zone="9", parking="Gratis", saludable="S", timing="Rapida")
 r7 = db.nodes.create(name="Los Cebollines", price="Alto", type1="Comida mexicana", tel="2256 2490", zone="11", parking="Pagado", saludable="S", timing="Tenedor")
 r8 = db.nodes.create(name="Del Griego", price="Alto", type1="Comida Griega", tel="5945 0000", zone="10", parking="Gratis", saludable="S", timing="Tenedor")
-r9 = db.nodes.create(name="Jacks Place", price="Medio", type1="Bar", tel="2474 5808", zone="11", parking="Pagado", saludable="NS", timing="Rapida")
+r9 = db.nodes.create(name="Jacks Place", price="Medio", type1="Bar", tel="2474 5808", zone="5", parking="Pagado", saludable="NS", timing="Rapida")
 #Agregamos todos los restaurantes
 restaurantes.add(r1, r2, r3, r4, r5, r6, r7, r8, r9)
 
@@ -97,7 +97,7 @@ u3.relationships.create("le gsuta", r3)
 #Menú
 menu = True
 while menu:
-        print ("\tLa base de este proye cto muestra relaciones de los usuarios con los restauramtes que le gustan")
+        print ("\tLa base de este proyecto muestra relaciones de los usuarios con los restauramtes que le gustan")
         print ("Desde aquí puedes seleccionar lo que desea hacer con el sistema de recoendaciones\n")                         
         print ("1.)Agregar nuevo usuario: ")                         
         print ("2.)Agregar nuevo Restaurante: ")                         
@@ -125,10 +125,18 @@ while menu:
              type1 = type1.capitalize()
              consultaTipo(type1,db)
         elif numero == "5":
-        	type1 = input("Ingresar el tipo de restaurante que necesita: \nbEjemplo Comida Rapida, Comida mexicana")
+        	type1 = input("Ingresar el tipo de restaurante que necesita: \n Ejemplo Comida Rapida, Comida mexicana")
         	type1 = type1.capitalize()
-        	price = input("Ingresar el precio que necesita: \nbusque Bajo, Medio o Alto: ")
+        	price = input("Ingresar el precio que necesita: \n busque Bajo, Medio o Alto: ")
         	price = price.capitalize()
+        	zone = input("Ingrese la zona de restaurante que necesita (de 1 a 16)")
+        	zone = price.capitalize()
+        	parking = input("Ingresar un tipo de parqueo: Gratis o Pagado")
+        	parking = paring.capitalizae()
+        	timing = input("Ingresar que tipo de comida: Rapida o Tenedor")
+        	timing = timing.capitalize()
+        	saludable = input("Ingresar que tipo de comida desea: saludable (S) o no saludable (NS)")
+        	saludable = saludable.capitalize()
         	recomendar(type1,price,client,zone,parking,timing,saludable,db)
         else:
             menu = False
